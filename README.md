b6176f2321f6a3cbb05978237c6ed545af8d3b97
root <root@LAPTOP-ELVLKBQU.localdomain>
2026-05-27 23:22:01 -0400

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# An inverted pendulum car robot
+This project holds the source code for a inverted pendulum car robot. The code is written primarly in cpp and is intended to be used with the mjbots family of tools. 
+
---
+++ src/constants.h
@@ -0,0 +1,20 @@
+// Constants.h
+#pragma once
+
+namespace config {
+    inline constexpr int RIGHT_MOT_ID = 2;
+    inline constexpr int RIGHT_MOT_RATIO = 10;
+
+    inline constexpr int LEFT_MOT_ID = 1;
+    inline constexpr int LEFT_MOT_RATIO = 8;
+
+    // pitch pid gains
+    inline constexpr int PITCH_KP = 0;
+    inline constexpr int PITCH_KI = 0;
+    inline constexpr int PITCH_KD = 0;
+
+    // yaw pid gains
+    inline constexpr int YAW_KP = 0;
+    inline constexpr int YAW_KI = 0;
+    inline constexpr int YAW_KD = 0;
+}
